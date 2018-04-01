7e402bcc14d6d39b5b23748cd891a4596aad44f7
Jonas Talavera <jonastg@gmail.com>
2018-04-01 21:35:00 +0200

    Add solution to kata 64 - set delete

---
+++ src/set/064-delete/solution.js
@@ -0,0 +1,51 @@
+// 64: Set - delete (solution)
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`set.delete()` deletes an element from a set', function(){
+
+  let set;
+  beforeEach(() => set = new Set());
+
+  describe('use `delete(<value>)` to delete an element', function() {
+    beforeEach(function() {
+      set.add('one').add('two').add('three');
+    });
+    it('`delete()` returns `true` when the element was found', function() {
+      const returns = set.delete('three');
+      assert.strictEqual(returns, true);
+    });
+    it('and the size decreases', function() {
+      set.delete('three');
+      assert.equal(set.size, 2);
+    });
+  });
+
+  describe('if nothing was deleted (no element with the given value was found)', function() {
+    it('returns `false`', function() {
+      set.add('one');
+      const returns = set.delete('x');
+
+      assert.equal(returns, false);
+    });
+  });
+
+  describe('`undefined` is a valid value in a set', function() {
+    it('deleting it, when it is not in the set, returns `false` too', function() {
+      const whatToDelete = void 0;
+      assert.equal(set.delete(whatToDelete), false);
+    });
+
+    it('`delete()` removes it, when its in the set', function() {
+      set.add(void 0);
+      assert.equal(set.delete(), true);
+    });
+  });
+
+
+  describe('the value does NOT get casted', function() {
+    it('number 1 is different to string "1"', function() {
+      set.add(1);
+      assert.equal(set.delete('1'), false);
+    });
+  });
+});
